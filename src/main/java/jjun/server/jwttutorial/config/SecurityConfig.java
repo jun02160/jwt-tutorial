7bd7fc44f889f090a3e3911ae196af07fcedd5c1
jun02160 <jun020216@naver.com>
2023-01-01 19:05:05 +0900

    Initial commit

---
+++ src/main/java/jjun/server/jwttutorial/config/SecurityConfig.java
@@ -0,0 +1,7 @@
+package jjun.server.jwttutorial.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity   // 기본적인 Web 보안 활성화
+public class SecurityConfig {
+}
