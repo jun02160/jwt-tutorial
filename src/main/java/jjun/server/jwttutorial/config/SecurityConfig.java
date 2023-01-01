708e6e40d20a55069e590cad877d04797420493c
jun02160 <jun020216@naver.com>
2023-01-01 19:26:08 +0900

    Security Config 설정

--- src/main/java/jjun/server/jwttutorial/config/SecurityConfig.java
+++ src/main/java/jjun/server/jwttutorial/config/SecurityConfig.java
@@ -1,7 +1,45 @@
 package jjun.server.jwttutorial.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 
+
+@Configuration
 @EnableWebSecurity   // 기본적인 Web 보안 활성화
+@EnableMethodSecurity
 public class SecurityConfig {
+
+
+    /**
+     * Spring Security 5.7x 부터 WebSecurityConfigureAdapter 는 Deprecated.
+     * -> SecurityFilterChain, WebSecurityCustomizer 를 상황에 따라 빈으로 등록해 사용한다.
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests((authz) -> authz
+                .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .logout().logoutSuccessUrl("/")
+//                .and()
+//                .rememberMe().userDetailsService().tokenRepository(tokenRepository())
+                .and().build();
+
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
 }
