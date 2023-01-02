935c1d8d160c195c0a5fabcaeded72ce9bc5a706
jun02160 <jun020216@naver.com>
2023-01-02 20:19:54 +0900

    [feat] Security Config 추가 및 Unauthorized, Forbidden 에러 리턴 클래스 커스텀

--- src/main/java/jjun/server/jwttutorial/config/SecurityConfig.java
+++ src/main/java/jjun/server/jwttutorial/config/SecurityConfig.java
@@ -1,5 +1,9 @@
 package jjun.server.jwttutorial.config;
 
+import jjun.server.jwttutorial.jwt.JwtAccessDeniedHandler;
+import jjun.server.jwttutorial.jwt.JwtAuthenticationEntryPoint;
+import jjun.server.jwttutorial.jwt.JwtSecurityConfig;
+import jjun.server.jwttutorial.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
@@ -21,10 +25,16 @@ import org.springframework.web.filter.CorsFilter;
 @EnableMethodSecurity
 public class SecurityConfig {
 
+    private final TokenProvider tokenProvider;
     private final CorsFilter corsFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
-    public SecurityConfig(CorsFilter corsFilter) {
+    public SecurityConfig(TokenProvider tokenProvider, CorsFilter corsFilter, JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint, JwtAccessDeniedHandler jwtAccessDeniedHandler) {
+        this.tokenProvider = tokenProvider;
         this.corsFilter = corsFilter;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
     }
 
     @Bean
@@ -44,11 +54,12 @@ public class SecurityConfig {
 
                 .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
 
-//                .exceptionHandling()
-//                .authenticationEntryPoint()
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
 
                 // enable h2-console
-//                .and()
+                .and()
                 .headers()
                 .frameOptions()
                 .sameOrigin()
@@ -62,7 +73,10 @@ public class SecurityConfig {
                 .authorizeHttpRequests()
                 .requestMatchers("/api/test").permitAll()
                 .requestMatchers(PathRequest.toH2Console()).permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
 
         return http.build();
     }
