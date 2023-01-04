b73554af6423b9751553e0caa8ccf11bccf2c970
jun02160 <jun020216@naver.com>
2023-01-05 01:47:42 +0900

    [fix] Refresh Token 추가 -> TokenProvider 수정

--- src/main/java/jjun/server/jwttutorial/jwt/TokenProvider.java
+++ src/main/java/jjun/server/jwttutorial/jwt/TokenProvider.java
@@ -3,6 +3,8 @@ package jjun.server.jwttutorial.jwt;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import jjun.server.jwttutorial.dto.TokenDto;
+import jjun.server.jwttutorial.dto.token.TokenResponseDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
@@ -26,15 +28,17 @@ public class TokenProvider implements InitializingBean {
     private final Logger logger = LoggerFactory.getLogger(TokenProvider.class);
     private static final String AUTHORITIES_KEY = "auth";
     private static final String BEARER_TYPE = "bearer";
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24;   // Access Token 만료 기한: 1일
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // Refresh Token 만료 기한: 7일
     private final String secret;
-    private final long tokenValidityInMilliseconds;
+//    private final long tokenValidityInMilliseconds;
     private Key key;
 
     public TokenProvider(
             @Value("${jwt.secret}") String secret,
             @Value("${jwt.token-validity-in-seconds}") long tokenValidityInMilliseconds) {
         this.secret = secret;
-        this.tokenValidityInMilliseconds = tokenValidityInMilliseconds * 1000;   // 만료기간 하루로 설정
+//        this.tokenValidityInMilliseconds = tokenValidityInMilliseconds * 1000;   // 만료기간 하루로 설정
     }
 
     // bean 생성 이후 주입을 받은 후, secret 값을 Base64로 decode 한 후에 key 변수에 넣어주는 작업
@@ -45,32 +49,46 @@ public class TokenProvider implements InitializingBean {
     }
 
     // Authentication 객체의 권한 정보를 이용해서 토큰을 생성하는 createToken 메소드
-    public String createToken(Authentication authentication) {
+    public TokenResponseDto createToken(Authentication authentication) {
         String authorities = authentication.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.joining(","));
 
         long now = (new Date()).getTime();   // 토큰의 만료 시간 설정
-        Date validity = new Date(now + this.tokenValidityInMilliseconds);
+        Date accessTokenExpireTime = new Date(now + ACCESS_TOKEN_EXPIRE_TIME);
 
-        // JWT 토큰을 생성하여 리턴
-        return Jwts.builder()
-                .setSubject(authentication.getName())
-                .claim(AUTHORITIES_KEY, authorities)
-                .signWith(key, SignatureAlgorithm.HS512)
-                .setExpiration(validity)
+        // JWT Access Token 생성
+        String accessToken = Jwts.builder()
+                .setSubject(authentication.getName())    // payload "sub" : "name"
+                .claim(AUTHORITIES_KEY, authorities)     // payload "auth" : "ROLE_USER"
+                .signWith(key, SignatureAlgorithm.HS512) // header "alg" : HS512 (해싱 알고리즘)
+                .setExpiration(accessTokenExpireTime)    // payload "exp" (10자리)
                 .compact();
+
+        // JWT Refresh Token 생성
+        String refreshToken = Jwts.builder()
+                .setExpiration(new Date(now + REFRESH_TOKEN_EXPIRE_TIME))
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+
+        return TokenResponseDto.builder()
+                .grantType(BEARER_TYPE)
+                .accessToken(accessToken)
+                .accessTokenExpiresIn(accessTokenExpireTime.getTime())
+                .refreshToken(refreshToken)
+                .build();
     }
 
-    // Token에 담겨 있는 정보를 이용해 Authentication 객체를 리턴하는 메소드
-    public Authentication getAuthentication(String token) {
-        Claims claims = Jwts   // 토큰을 파라미터로 받아 클레임 생성
-                .parserBuilder()
-                .setSigningKey(key)
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
+    // Token 에 담겨 있는 정보를 이용해 Authentication 객체를 리턴하는 메소드
+    public Authentication getAuthentication(String accessToken) {
 
+        Claims claims = parseClaims(accessToken);
+
+        if (claims.get(AUTHORITIES_KEY) == null) {
+            throw new RuntimeException("권한 정보가 없는 토큰입니다.");
+        }
+
+        // Claim 으로 권한 정보 가져오기
         Collection<? extends GrantedAuthority> authorities =
                 Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
                         .map(SimpleGrantedAuthority::new)
@@ -79,7 +97,7 @@ public class TokenProvider implements InitializingBean {
         // 유저 객체를 만들어 최종적으로 Authentication 객체를 리턴
         User principal = new User(claims.getSubject(), "", authorities);
 
-        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
+        return new UsernamePasswordAuthenticationToken(principal, "", authorities);
     }
 
     // 토큰의 유효성 검증을 수행하는 메소드
@@ -98,6 +116,19 @@ public class TokenProvider implements InitializingBean {
         }
 
         return false;
+    }
+
+    // 토큰을 파라미터로 받아 클레임 생성
+    private Claims parseClaims(String accessToken) {
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(accessToken)
+                    .getBody();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        }
     }
 
 }
